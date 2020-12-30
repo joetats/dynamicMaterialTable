97c3508551da9305b7aade339787e96e8a2feb4f
joetats <tatuskojc@gmail.com>
2020-12-30 14:45:23 -0500

    initial setup

---
+++ dynamic-table/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+  </div>
+</template>
+
+<script>
+import TableCard from "./components/TableCard.vue";
+
+export default {
+  name: "App",
+  components: {
+    TableCard,
+  },
+};
+</script>
+
+<style>
+</style>
